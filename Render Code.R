9cd8640b9cb41d5a632f528aef731be43deccb5b
Mary Brown <mbrown29@ncsu.edu>
2021-10-30 14:55:15 -0400

    Update

--- Render Code.R
+++ Render Code.R
@@ -1,5 +1,5 @@
 library(rmarkdown)
-rmarkdown::render("~/ST558/Project_2/Project-2-558/Project 2.Rmd",
+rmarkdown::render("~/Desktop/R Programming/iCloud Drive (Archive)/Project-2-558/Project 2.Rmd",
                   output_format = "github_document",
                   output_file = "README.md"  
 )
