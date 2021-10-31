3b15cdae2b03b8306f35f0c75e2af11ecdea689a
jlrelihan <jlreliha@ncsu.edu>
2021-10-31 13:19:51 -0400

    Updated render code to include render function for automation of all 6 channel documents.

--- Render Code.R
+++ Render Code.R
@@ -1,5 +1,12 @@
+# Code to render the .Rmd file to the readme.md
 library(rmarkdown)
-rmarkdown::render("~/Desktop/R Programming/iCloud Drive (Archive)/Project-2-558/Project 2.Rmd",
+rmarkdown::render("~/ST558/Project_2/Project-2-558/Project 2.Rmd",
                   output_format = "github_document",
-                  output_file = "README.md"  
-)
+                  output_file = "README.md")
+
+
+# Updated render function for automation of all 6 channel documents
+apply(reports, MARGIN=1,
+      FUN = function(x){
+        render(input = "~/ST558/Project_2/Project-2-558/Project 2.Rmd", output_file= x[[1]], params = x[[2]])
+      })
