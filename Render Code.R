71add88a5f57b1fef17f6d9bb2d079a798dc4770
Mary Brown <mbrown29@ncsu.edu>
2021-10-31 17:34:40 -0400

    Changes

--- Render Code.R
+++ Render Code.R
@@ -1,6 +1,6 @@
 # Code to render the .Rmd file to the readme.md
 library(rmarkdown)
-rmarkdown::render("~/ST558/Project_2/Project-2-558/Project 2.Rmd",
+rmarkdown::render("~/Desktop/R Programming/iCloud Drive (Archive)/Project-2-558/Project-2-558/Project 2.Rmd",
                   output_format = "github_document",
                   output_file = "README.md")
 
@@ -8,5 +8,5 @@ rmarkdown::render("~/ST558/Project_2/Project-2-558/Project 2.Rmd",
 # Updated render function for automation of all 6 channel documents
 apply(reports, MARGIN=1,
       FUN = function(x){
-        render(input = "~/ST558/Project_2/Project-2-558/Project 2.Rmd", output_file= x[[1]], params = x[[2]])
+        render(input = "~/Desktop/R Programming/iCloud Drive (Archive)/Project-2-558/Project 2.Rmd", output_file= x[[1]], params = x[[2]])
       })
